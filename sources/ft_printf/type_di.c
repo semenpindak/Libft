d408951bb4f047470dfa7fbe79c70ab8122ab287
semenpindak <pindak@inbox.ru>
2020-11-29 17:28:31 +0300

    updating type_di

--- sources/ft_printf/type_di.c
+++ sources/ft_printf/type_di.c
@@ -6,7 +6,7 @@
 /*   By: oem <oem@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 15:13:21 by oem               #+#    #+#             */
-/*   Updated: 2020/11/28 03:36:14 by oem              ###   ########.fr       */
+/*   Updated: 2020/11/29 17:24:33 by oem              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -225,7 +225,112 @@ int	type_di(t_field f, va_list args)
 	}
 	if ((f.minus == 1 || f.plus == 1 || f.space == 1 || f.zero == 1) && f.width != 0 && (f.dot == 1 || f.precision != 0))
 	{
-
+		i = ft_strlen(s);
+		if ((size_t)i >= (size_t)f.width && (size_t)i >= (size_t)f.precision && (size_t)f.width >= f.precision)
+		{
+			if (f.plus == 1)
+				n = write(1, "+", 1);
+			if (f.space == 1)
+				n = write(1, " ", 1);
+			n += ft_putstr(s);
+		}
+		if ((size_t)i >= (size_t)f.width && (size_t)i >= (size_t)f.precision && (size_t)f.width < f.precision)
+		{
+			if (f.plus == 1)
+				n = write(1, "+", 1);
+			if (f.space == 1)
+				n = write(1, " ", 1);
+			n += ft_putstr(s);
+		}
+		if ((size_t)i >= (size_t)f.width && (size_t)i < (size_t)f.precision && (size_t)f.width < f.precision)
+		{
+			if (f.plus == 1)
+				n = write(1, "+", 1);
+			if (f.space == 1)
+				n = write(1, " ", 1);
+			f.precision -= i;
+			while (f.precision--)
+				n += write(1, "0", 1);
+			n += ft_putstr(s);
+		}
+		if ((size_t)i < (size_t)f.width && (size_t)i >= (size_t)f.precision && (size_t)f.width >= f.precision)
+		{
+			f.width -= i;
+			if (f.plus == 1 || f.space == 1)
+				f.width--;
+			if (f.minus == 0)
+			{
+				while (f.width--)
+					n += write(1, " ", 1);
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+			}
+			if (f.minus == 1)
+			{
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+			}
+			n += ft_putstr(s);
+		}
+		if ((size_t)i < (size_t)f.width && (size_t)i < (size_t)f.precision && (size_t)f.width >= f.precision)
+		{
+			if (f.minus == 0)
+			{
+				f.width -= f.precision;
+				if (f.space == 1)
+					f.width--;
+				if (f.plus == 1)
+					f.width--;
+				while (f.width--)
+					n += write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				f.precision -= i;
+				while (f.precision--)
+					n += write(1, "0", 1);
+			}
+			if (f.minus == 1)
+			{
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+				f.precision -= i;
+				while (f.precision--)
+					n += write(1, "0", 1);
+			}
+			n += ft_putstr(s);
+		}
+		if ((size_t)i < (size_t)f.width && (size_t)i < (size_t)f.precision && (size_t)f.width < f.precision)
+		{
+			if (f.minus == 0)
+			{
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+				f.precision -= i;
+				while (f.precision--)
+					n += write(1, "0", 1);
+			}
+			if (f.minus == 1)
+			{
+				if (f.space == 1)
+					n = write(1, " ", 1);
+				if (f.plus == 1)
+					n = write(1, "+", 1);
+				f.precision -= i;
+				while (f.precision--)
+					n += write(1, "0", 1);
+			}
+			n += ft_putstr(s);
+		}
 	}
 
 	return (n);
